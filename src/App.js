65142eb08d502fb6b84256a8f1ad31b36c41b4ce
Luis-f4 <luisbittner1.0@gmail.com>
2024-09-09 11:29:02 +0200

    Popup in App.js Hinzugefuegt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import './TableLicense'
 import TableLicense from './TableLicense';
+import Popup from './Popup';
 function App() {
   return (
     <div className="App">
       <TableLicense />
+      <Popup />
     </div>
   );
 }
---
+++ src/Popup.js
@@ -0,0 +1,11 @@
+const Popup = () => {
+    return ( 
+
+    <div id="PopupMainDiv">
+
+    </div> 
+    
+);
+}
+ 
+export default Popup;
