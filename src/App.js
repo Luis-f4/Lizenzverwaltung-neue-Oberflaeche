9ed0057bc4a6eac51d54c87d27bb22c03c62023c
JanTaco <Jan.wegner2002@gmail.com>
2024-09-09 09:55:21 +0200

    Test 2 von Jan

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // Test 1 von Jan
-  // Test 2 von Luis
   return (
     <div className="App">
       <header className="App-header">
